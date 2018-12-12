57b21774bf80100e164fafce0ac3a4afad5b5aab
Tim Graf <tim@tim-graf.de>
2018-12-12 22:39:57 +0100

    add documentation for word function

--- R/word-service.R
+++ R/word-service.R
@@ -3,10 +3,20 @@
 # Function names correspond to the API names
 # The German corpus will be used as a Default
 
-
-
-
-
+#' Queries a lists of information about a word in a corpus.
+#'
+#' @param word A string or vector of strings containing words, for which the information should be queried
+#' @param corpus Name of the corpus to query. Default is the German deu_news_2012_1M
+#' @param ... Additional arguments passed to catch_error_json. For now quietly = FALSE enables the display of warnings,
+#'  whenever a word has not entry in the corpus
+#'
+#' @return A tibble with the information about the queried words. That is Frequency, FrequencyClass and WordRank
+#' @export
+#'
+#' @examples
+#'
+#' word(c("Hund", "Katze", "Maus"))
+#'
 word <- function(word, corpus = "deu_news_2012_1M", ...){
 
     assertthat::assert_that(all(is.character(word)),
