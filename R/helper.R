168f1e607586c5bf4e46ebc72bbb09f9b8743450
Tim Graf <tim@tim-graf.de>
2018-12-13 21:01:05 +0100

    make the catch_error_json more general

--- R/helper.R
+++ R/helper.R
@@ -5,22 +5,11 @@
 #' @param txt String: URL to the JSON file
 #' @param quietly If FALSE, a warning message is returned, whenever an error is encountered. Default: TRUE
 #'
-#' @return A list with the query results or a list with NAs for each variable that was requested
+#' @return A list with the query results and the potential error message
 
 catch_error_json <- function(txt, quietly = TRUE){
     safe_json <- purrr::safely(jsonlite::fromJSON)
     query_result <- safe_json(txt)
-
-    if (is.null(query_result$error))
-    {
-        return(query_result$result)
-    } else {
-        word_string <- stringr::str_extract(txt, "\\w+$")
-        if (!quietly) warning(glue::glue("There is no record of the word {word_string}"))
-        return(list(id = NA,
-                    word = word_string,
-                    freq = NA,
-                    wordRank = NA,
-                    frequencyClass = NA))
+        if (!quietly & !is.null(query_result$error)) warning(glue::glue("There is no record of the word {word_string}"))
+    return(query_results)
     }
-}
